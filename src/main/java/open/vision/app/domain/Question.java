4f49e9cc9e8b1ebfe36645215a46d23f9f1f6d84
jea <a1800601@myy.haaga-helia.fi>
2019-04-08 08:22:24 +0300

    toimii ko?

---
+++ src/main/java/open/vision/app/domain/Question.java
@@ -0,0 +1,85 @@
+package open.vision.app.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import open.vision.app.domain.Answer;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+
+
+
+
+@Entity
+public class Question {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String title;
+	
+	@ManyToOne
+	@JsonIgnore
+	@JoinColumn(name="answerid")
+	private Answer answer;
+	
+	
+	
+	public Question() {
+		super();
+		this.id = null;
+		this.title = null;
+	}
+
+
+	public Question(String title) {
+		super();
+		this.title = title;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+	
+
+
+	public Answer getAnswer() {
+		return answer;
+	}
+
+
+	public void setAnswer(Answer answer) {
+		this.answer = answer;
+	}
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+	@Override
+	public String toString() {
+		if(this.answer != null) {
+			return "Question [id=" + id + ", title=" + title + "answer =" + this.getAnswer() + "]";
+		} else {
+			return "Question [id=" + id + ", title=" + title + "]";
+		}
+		
+	}
+}
